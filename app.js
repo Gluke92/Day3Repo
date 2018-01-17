8962ed767c4f95291ea3f4f774e370f3317f71f3
George Luke <george.luke92@gmail.com>
2018-01-17 15:19:57 -0600

    done up to problem 6

--- app.js
+++ app.js
@@ -605,3 +605,98 @@ console.log(validateKeys(objectB, expectedKeys));
 // }
 
 // testIt();
+
+
+// Afternoon Problem set
+
+// Problem 1
+
+// 1. Create an object called loaf using an object initializer({}) with two properties: flour, which should be set to 300 
+//and water which should be set to 210.
+
+// 2. Use console.log to print the flour and water properties.
+
+// 3. Add an empty method to the loaf object called hydration.
+
+// 4. Fill in the body of the method to
+// return the hydration of the loaf(the water divided by the flour multiplied by 100).
+// 5. Call the hydration method and use console.log to print the result.
+
+const loaf = {
+  flour: 300,
+  water: 210,
+  hydration: function() {
+    return (loaf.water / loaf.flour) * 100;
+  }
+}; 
+
+console.log(loaf.hydration());
+
+// Problem 2
+
+// Create an object with five properties: foo, bar, fum, quux, and spam.
+// Give each property a unique value of your choosing.
+// Loop over the object using
+// for... in
+// Use console.log to show each property name and its associated value.
+
+const fiveProp = {
+  foo: 20,
+  bar: 'stuff',
+  fum: 'moe',
+  quux: 'xuuq',
+  spam: 'egg'
+}
+
+for (let k in fiveProp) {
+  console.log(`The name is ${k} and I hold ${fiveProp[k]}`);
+}
+
+// Problem 3
+
+const hobbitEats = {
+  meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+};
+
+console.log(hobbitEats.meals[3]);
+
+// Problem 4
+
+// Create 3 - 5 objects, each with a name and a jobTitle.Use people you know, or characters from fiction, or your own inventions.
+// Store these objects in an array.
+// Iterate over the array and use console.log to show each person 's job title and name.
+
+const harryPotter= {
+  name: 'Harry',
+  title: 'Auror'
+}
+
+const hermioneGranger = {
+  name: 'Hermione',
+  title: 'Headmaster',
+  boss: 'Harry'
+}
+
+const ronWeasley = {
+  name: 'Ron',
+  title: 'Redhead',
+  boss: 'Harry'
+}
+
+const hogwarts = [];
+
+hogwarts.push(harryPotter, hermioneGranger, ronWeasley);
+// Problem 5
+for (let k in hogwarts) { 
+  if ( hogwarts[k].boss === undefined){
+    console.log(`${hogwarts[k].title} ${hogwarts[k].name}  doesn't report to anybody.`);
+  }
+  console.log(`${hogwarts[k].title} ${hogwarts[k].name} reports to ${hogwarts[k].boss}`);
+}
+
+
+
+
+
+
+
